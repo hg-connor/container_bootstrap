65c3fefa31b398e23dc348c28287d031dc4f4223
Connor Hollis <connor@hemme.games>
2020-09-03 18:02:37 -0700

    Adding start minecraft script

---
+++ start_minecraft.sh
@@ -0,0 +1,29 @@
+# Install docker
+yum install -y docker
+
+# Start the docker service
+systemctl start docker
+
+# Remove the existing docker DL
+rm /usr/local/bin/docker-compose
+rm /usr/bin/docker-compose
+
+# Download docker compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Mark as executable
+chmod +x /usr/local/bin/docker-compose
+
+# Symbolic link to the bin dir
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Pull from repo if it is already present, otherwise clone
+mkdir /hemme
+cd /hemme
+git clone https://github.com/hg-connor/minecraft_server.git
+git pull
+
+cd /hemme/minecraft_server
+
+# Start docker compose script which will build the website and start it
+docker-compose up -d
